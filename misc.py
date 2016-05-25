7e5f0c0f354f192a8228dceb3fe8a067aeb12f58
Oleg Golovanov <golovanov.ov@gmail.com>
2016-05-25 14:15:44 +0500

    Небольшие изменения в функции int_to_bytes.

--- misc.py
+++ misc.py
@@ -159,10 +159,12 @@ def int_to_bytes(num, count=None):
     if count:
         bytes_count = count
     else:
-        q, r = divmod(num.bit_length(), 8)
-        bytes_count = q + 1 if r else q
+        if num == 0:
+            bytes_count = 1
+        else:
+            q, r = divmod(num.bit_length(), 8)
+            bytes_count = q + 1 if r else q
 
-    # TODO: реализоваться возврат (0, ) при входном 0
     return tuple((num >> (8 * i)) & 0xff for i in xrange(bytes_count))
 
 
