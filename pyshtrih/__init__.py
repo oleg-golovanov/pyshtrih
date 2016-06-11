48d356c8defcd6b7fc90954c85ab77738d146adc
Oleg Golovanov <golovanov.ov@gmail.com>
2016-06-12 02:48:05 +0500

    Поправлены импорты.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -2,15 +2,15 @@
 
 
 from .protocol import Protocol
-from .driver import Driver
-from .device import Device, ShtrihFRK, ShtrihComboFRK, ShtrihComboPTK
+from .device import ShtrihFRK, ShtrihComboFRK, ShtrihComboPTK, ShtrihAllCommands
 from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, Error, CheckError, \
     OpenCheckError, ItemSaleError, CloseCheckError, NoItemsError
 
 
 __version__ = '1.0.0'
 __all__ = (
-    'Protocol', 'Driver', 'Device', 'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ProtocolError',
-    'NoConnectionError', 'UnexpectedResponseError', 'Error', 'CheckError', 'OpenCheckError', 'ItemSaleError',
-    'CloseCheckError', 'NoItemsError'
+    'Protocol',
+    'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ShtrihAllCommands',
+    'ProtocolError', 'NoConnectionError', 'UnexpectedResponseError', 'Error', 'CheckError',
+    'OpenCheckError', 'ItemSaleError', 'CloseCheckError', 'NoItemsError'
 )
