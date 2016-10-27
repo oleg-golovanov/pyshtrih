479376ab252a79b90a9baf223ad6c74d381e220b
Oleg Golovanov <golovanov.ov@gmail.com>
2016-10-27 18:38:00 +0500

    Выпуск версии 1.3.0.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -7,7 +7,7 @@ from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, 
     OpenCheckError, ItemSaleError, CloseCheckError
 
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __all__ = (
     'Protocol',
     'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ShtrihAllCommands',
