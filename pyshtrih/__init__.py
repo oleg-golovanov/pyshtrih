03050b4945720249072971811260304bbbc075ad
Oleg Golovanov <golovanov.ov@gmail.com>
2016-11-10 17:59:05 +0500

    Выпуск версии 1.4.1.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
+from .utils import discovery
 from .protocol import Protocol
 from .device import ShtrihFRK, ShtrihComboFRK, ShtrihComboPTK, ShtrihAllCommands
 from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, Error, CheckError, \
     OpenCheckError, ItemSaleError, CloseCheckError
 
 
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 __all__ = (
+    'discovery',
     'Protocol',
     'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ShtrihAllCommands',
     'ProtocolError', 'NoConnectionError', 'UnexpectedResponseError', 'Error', 'CheckError',
