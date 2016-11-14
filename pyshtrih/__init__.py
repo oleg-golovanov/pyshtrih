8f869ab090dd38c9241fc242f043f0ee68f4bca1
Oleg Golovanov <golovanov.ov@gmail.com>
2016-11-14 11:17:09 +0500

    Выпуск версии 1.4.5.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -8,7 +8,7 @@ from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, 
     OpenCheckError, ItemSaleError, CloseCheckError
 
 
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 __all__ = (
     'discovery',
     'Protocol',
