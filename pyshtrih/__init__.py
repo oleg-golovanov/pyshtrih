752334ebf5ad4b00f605f63ee9a8053820bc2382
Oleg Golovanov <golovanov.ov@gmail.com>
2016-12-01 15:00:05 +0500

    Выпуск версии 1.5.2.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -8,7 +8,7 @@ from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, 
     OpenCheckError, ItemSaleError, CloseCheckError
 
 
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 __all__ = (
     'discovery',
     'Protocol',
