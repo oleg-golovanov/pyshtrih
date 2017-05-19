6cc8554af33154fb0738e7d4fa65f4edae1b5a1d
Oleg Golovanov <golovanov.ov@gmail.com>
2017-05-19 18:35:10 +0500

    Выпуск версии 1.8.4.

--- pyshtrih/__init__.py
+++ pyshtrih/__init__.py
@@ -10,7 +10,7 @@ from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, 
 from .fd import FD
 
 
-__version__ = '1.8.3'
+__version__ = '1.8.4'
 __all__ = (
     'discovery',
     'Protocol',
